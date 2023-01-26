ece5437448d97afc4cabed4434d9c7bd17eb21a5
Ncuti Xavier <51375428+ncutixavier@users.noreply.github.com>
2023-01-26 16:13:54 +0200

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://leave-app-sys.herokuapp.com/api/v1/coinmarket',
+  baseURL: 'https://api-cors-resolver.onrender.com/api/v1/coinmarket',
   headers: {
     'Content-type': 'application/json',
   },
